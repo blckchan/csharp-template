44b317b8c52079d0e7c243a11ae4d1a1847e5f09
blckchan <1258176714@qq.com>
2018-10-08 19:49:33 -0700

    rename dynamic call api interface

--- API/DynamicCallOep4.cs
+++ API/DynamicCallOep4.cs
@@ -6,36 +6,35 @@ namespace Contract
 {
     public class Contract : SmartContract
     {
-        public delegate object NEP5Contract(string method, object[] args);
+        public delegate object OEP4Contract(string method, object[] args);
 
         public static Object Main(string operation, params object[] args)
         {
-            if (operation == "CallNep5Contract")
+            if (operation == "CallOep4Contract")
             {
                 if (args.Length != 4) return false;
                 byte[] from = (byte[])args[0];
                 byte[] to = (byte[])args[1];
                 ulong value = (ulong)args[2];
                 byte[] hash = (byte[])args[3];
-                return CallNep5Contract(from, to, value, hash);
+                return CallOep4Contract(from, to, value, hash);
             }
             return false;
         }
        
-        public static bool CallNep5Contract(byte[] from, byte[] to, ulong value, byte[] contractHash)
+        public static bool CallOep4Contract(byte[] from, byte[] to, ulong value, byte[] contractHash)
         {
        		
-            if (!TransferNEP5(from, to, contractHash, value)) throw new Exception();
+            if (!TransferOEP4(from, to, contractHash, value)) throw new Exception();
             return true;
         }
 
-        private static bool TransferNEP5(byte[] from, byte[] to, byte[] assetID, BigInteger amount)
+        private static bool TransferOEP4(byte[] from, byte[] to, byte[] assetID, BigInteger amount)
         {
             var args = new object[] { from, to, amount };
-            var contract = (NEP5Contract)assetID.ToDelegate();
+            var contract = (OEP4Contract)assetID.ToDelegate();
             if (!(bool)contract("transfer", args)) return false;
             return true;
         }
     }
 }
-
